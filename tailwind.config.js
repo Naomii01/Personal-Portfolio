6bfa13932c7503629bc725c3979d84e10e1e142c
Naomii01 <naomi222001220@gmail.com>
2024-03-18 03:50:26 +0200

    tailwi ccss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./Personal-Portfolio/index.html", "./src/input.css"],
+  theme: {
+    fontFamily :{
+      monstserrat: ['Montserrat','sans-serif'],
+      inter:['Inter','sans-serif'],
+      primary: ['Open Sans', 'sans-serif']
+
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
