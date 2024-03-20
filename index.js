330d4ae80c232ef25593430b273313991d699c67
Naomii01 <naomi222001220@gmail.com>
2024-03-20 04:06:28 +0200

    Update

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const body = document.body;
     const toggleSwitch = document.getElementById("toggle-switch");
-    const moonIcon = document.getElementById("moon");
-    const sunIcon = document.getElementById("sun");
 
     // Check local storage for saved theme
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
         body.classList.add(savedTheme);
-        // Update icons based on saved theme
-        if (savedTheme === 'dark') {
-            moonIcon.style.display = "block";
-            sunIcon.style.display = "none";
-        } else {
-            moonIcon.style.display = "none";
-            sunIcon.style.display = "block";
-        }
     }
 
     toggleSwitch.addEventListener("click", () => {
         // Toggle dark mode class on body
-        body.classList.toggle("dark");
+        body.classList.toggle("dark-theme");
+        body.classList.toggle("light-theme");
 
         // Save theme preference to local storage
-        const currentTheme = body.classList.contains("dark") ? "dark" : "";
+        const currentTheme = body.classList.contains("dark-theme") ? "dark-theme" : "light-theme";
         localStorage.setItem('theme', currentTheme);
-
-        // Update icons based on current theme
-        if (currentTheme === 'dark') {
-            moonIcon.style.display = "block";
-            sunIcon.style.display = "none";
-        } else {
-            moonIcon.style.display = "none";
-            sunIcon.style.display = "block";
-        }
     });
 });
